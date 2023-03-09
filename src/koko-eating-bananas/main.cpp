25af5e9486390a6d0572cb3fd69eab1322e5b40e
tu.dang <104347795+tudang-logix@users.noreply.github.com>
2023-03-09 13:17:04 +0700

    [LC] Commit

--- src/koko-eating-bananas/main.cpp
+++ src/koko-eating-bananas/main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+bool canEat(vector<int> &piles, int h, int speed)
+{
+    int time = 0;
+    for (int pile : piles)
+    {
+        time += (pile - 1) / speed + 1;
+        if (time > h)
+            return false;
+    }
+    return true;
+}
 
 int minEatingSpeed(vector<int> &piles, int h)
 {
-    return 10;
+    int left = 1;
+    int right = *max_element(piles.begin(), piles.end());
+    while (left < right)
+    {
+        int mid = (left + right) / 2;
+        if (canEat(piles, h, mid))
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+    return left;
 }
 
 int main()
