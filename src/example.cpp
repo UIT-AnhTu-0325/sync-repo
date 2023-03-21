e06ea363b4f010f21a3aaaa4a432c8c93de52913
tu.dang <104347795+tudang-logix@users.noreply.github.com>
2023-03-21 17:36:14 +0700

    Commit

---
+++ src/example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int i = 19;
+    int *a = &i;
+    int **b = &a;
+
+    cout << i << '\t' << &i << endl
+         << a << '\t' << *a << '\t' << &a << endl
+         << b << '\t' << *b << '\t' << **b << endl;
+}
