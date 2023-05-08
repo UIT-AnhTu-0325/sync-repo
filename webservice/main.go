c76b1d4950532ef28be48a08f68150c16f0b93a5
tu.dang <104347795+tudang-logix@users.noreply.github.com>
2023-05-08 17:51:37 +0700

    complete part 9

--- webservice/main.go
+++ webservice/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"context"
-	"example/webservice-gin/handler"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	log.Println("Starting server...")
 
-	router := gin.Default()
-	handler.NewHandler(&handler.Config{
-		R: router,
-	})
+	ds, err := initDS()
+
+	if err != nil {
+		log.Fatalf("Unable to initialize data sources: %v\n", err)
+	}
+
+	router, err := inject(ds)
+
+	if err != nil {
+		log.Fatalf("Failure to inject data sources: %v\n", err)
+	}
 
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -46,6 +50,11 @@ func main() {
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	// shutdown data sources
+	if err := ds.close(); err != nil {
+		log.Fatalf("A problem occurred gracefully shutting down data sources: %v\n", err)
+	}
 
 	// Shutdown server
 	log.Println("Shutting down server...")
