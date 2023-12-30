b1e7b0403b5504472b03c422493da285d78863bd
tudang <danganhtu2001@gmail.com>
2023-12-31 02:17:20 +0700

    Ok

--- src/merge-intervals/main.cpp
+++ src/merge-intervals/main.cpp
@@ -6,10 +6,32 @@ using namespace std;
 
 vector<vector<int>> merge(vector<vector<int>> &intervals)
 {
+    sort(intervals.begin(), intervals.end());
+
     vector<vector<int>> result = vector<vector<int>>();
     vector<int> item = vector<int>();
+
     for (int i = 0; i < intervals.size(); i++)
     {
+        if (item.size() == 0)
+        {
+            item.push_back(intervals[i][0]);
+            item.push_back(intervals[i][1]);
+        }
+        else
+        {
+            if (item[1] >= intervals[i][0])
+            {
+                item[1] = intervals[i][1] > item[1] ? intervals[i][1] : item[1];
+            }
+            else
+            {
+                result.push_back(item);
+                item = vector<int>();
+                item.push_back(intervals[i][0]);
+                item.push_back(intervals[i][1]);
+            }
+        }
     }
     result.push_back(item);
     return result;
