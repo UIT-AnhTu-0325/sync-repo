089184de31c377b0e7394f8a0e09b7bbc469dc7d
TuDang <tuda@galaxy.one>
2024-04-08 23:18:43 +0700

    Update solution - mac

---
+++ C++/remove-nth-node-from-end-of-list/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+    }
+};
+
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
