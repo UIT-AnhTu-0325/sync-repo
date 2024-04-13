34a5c41f73fb6e5bdc4af028f31bddd23ec87a82
tudang <danganhtu2001@gmail.com>
2024-04-13 16:16:32 +0700

    Update stack solution

---
+++ C++/largest-rectangle-in-histogram/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+// https://www.youtube.com/watch?v=jC_cWLy7jSI
+
+class Solution
+{
+public:
+    int largestRectangleArea(vector<int> &heights)
+    {
+    }
+};
+
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
---
+++ C++/trapping-rain-water/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    int trap(vector<int> &height)
+    {
+        int left = 0, right = 0;
+        while (right < height.size())
+        {
+        }
+    }
+};
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
