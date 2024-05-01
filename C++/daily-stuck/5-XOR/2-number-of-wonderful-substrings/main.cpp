dbf7ad7d9f8b773eb9ab646213cfcbcff6c265e4
TuDang <tuda@galaxy.one>
2024-05-01 10:51:50 +0700

    Need to solve many stuck :(

---
+++ C++/daily-stuck/5-XOR/2-number-of-wonderful-substrings/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    long long wonderfulSubstrings(string word)
+    {
+    }
+};
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
---
+++ C++/reverse-prefix-of-word/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    string reversePrefix(string word, char ch)
+    {
+    }
+};
+
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
