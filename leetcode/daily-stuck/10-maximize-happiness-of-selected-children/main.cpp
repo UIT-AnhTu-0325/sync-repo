b5a7c1de01f2a764a88a7d619d0b71b6326e131c
TuDang <tuda@galaxy.one>
2024-05-10 22:28:08 +0700

    Update daily stuck

---
+++ leetcode/daily-stuck/10-maximize-happiness-of-selected-children/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+// daily-question 2024-05-09
+class Solution
+{
+public:
+    long long maximumHappinessSum(vector<int> &happiness, int k)
+    {
+    }
+};
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
