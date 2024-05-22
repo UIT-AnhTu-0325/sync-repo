71eb3fdfe0641d8cfd949c6851d87e3d94eb5fd3
TuDang <tuda@galaxy.one>
2024-05-23 00:17:56 +0700

    update solution

---
+++ leetcode/palindrome-partitioning/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+// daily-question 2024-05-22
+class Solution
+{
+public:
+    vector<vector<string>> partition(string s)
+    {
+    }
+};
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
