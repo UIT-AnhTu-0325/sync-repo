21611bc273a7966b76c73bebbc10d8d080453732
TuDang <tuda@galaxy.one>
2024-06-21 00:14:57 +0700

    Solve all stuck soon

---
+++ leetcode/daily-stack/13-react-pending/magnetic-force-between-two-balls/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+// daily-question 2024-06-20
+class Solution
+{
+public:
+    int maxDistance(vector<int> &position, int m)
+    {
+    }
+};
+
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
---
+++ leetcode/daily-stack/13-react-pending/minimum-number-of-days-to-make-m-bouquets/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <queue>
+using namespace std;
+
+// daily-question 2024-06-19
+class Solution
+{
+public:
+    int minDays(vector<int> &bloomDay, int m, int k)
+    {
+    }
+};
+
+int main()
+{
+    Solution *solution = new Solution();
+    cout << "Hello world";
+}
