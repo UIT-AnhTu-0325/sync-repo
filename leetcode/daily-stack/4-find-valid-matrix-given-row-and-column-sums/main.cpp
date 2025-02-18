a1f8f0c66121e5732156d2218224aaff6f45331a
tuda-lap <danganhtu2001@gmail.com>
2025-02-18 10:24:44 +0700

    go go go power ranger

--- leetcode/daily-stack/4-find-valid-matrix-given-row-and-column-sums/main.cpp
+++ leetcode/daily-stack/4-find-valid-matrix-given-row-and-column-sums/main.cpp
@@ -13,11 +13,30 @@ using namespace std;
 class Solution {
   public:
     vector<vector<int>> restoreMatrix(vector<int> &rowSum, vector<int> &colSum) {
+      int m = rowSum.size();
+      int n = colSum.size();
 
+      int i = 0, j =0;
+      vector<vector<int>> res = vector<vector<int>>(m, vector<int>(n, 0));
+
+      while(i < m && j < n){
+        int value = min(rowSum[i], colSum[j]);
+        res[i][j] = value;
+        rowSum[i] -= value;
+        colSum[j] -= value;
+
+        if(rowSum[i] == 0) i++; 
+        if(colSum[j] == 0) j++;
+      }
+
+      return res;
     }
 };
 
 int main() {
     Solution *solution = new Solution();
+    vector<int> rowSum = vector<int>{3, 8};
+    vector<int> colSum = vector<int>{4, 7};
+    solution->restoreMatrix(rowSum, colSum);
     cout << "Hello world";
 }
