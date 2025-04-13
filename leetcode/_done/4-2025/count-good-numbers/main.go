2a374a5ae0bd16d057a7719e41e7f47a3253d4aa
tudang <danganhtu2001@gmail.com>
2025-04-13 12:24:01 +0700

    big update

---
+++ leetcode/_done/4-2025/count-good-numbers/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	_ = countGoodNumbers
+}
+
+func countGoodNumbers(n int64) int {
+	mod := int64(1000000007)
+
+	// use fast exponentiation to calculate x^y % mod
+	quickmul := func(x, y int64) int64 {
+		ret := int64(1)
+		mul := x
+		for y > 0 {
+			if y%2 == 1 {
+				ret = ret * mul % mod
+			}
+			mul = mul * mul % mod
+			y /= 2
+		}
+		return ret
+	}
+
+	return int(quickmul(5, (n+1)/2) * quickmul(4, n/2) % mod)
+}
